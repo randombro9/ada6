2251ac1f6c942bf424b5fb8155ccac4cf3b348b4
randombro9 <101424866+randombro9@users.noreply.github.com>
2022-06-30 00:33:19 +0530

    siwedfhw

--- p6.c
+++ p6.c
@@ -1,27 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
+#include <string.h>
+#include <stdio.h>
+char str[100],ptn[20];
+int res,m,n,len,len1,i,j,k,table[1000];
+void main()
 {
-FILE *fp;
-char ch;
-int num;
-long length;
-printf("Enter the value of num:");
-scanf("%d",&num);
-fp=fopen("file.txt","r");
-if(fp==NULL)
-{
-puts("Cannot open this file");
-exit(1);
+printf("Enter text \n");
+gets(str);
+printf("Enter pattern to be found \n");
+gets(ptn);
+res=horspool(ptn,str);
+if(res==-1)
+printf("\nPattern was not found\n");
+else
+printf("Pattern is found at %d position \n",res+1);
 }
-fseek(fp,-1,SEEK_END);
-length=ftell(fp);
-fseek(fp,(length-num),SEEK_SET);
-do
+void shift(char p[])
 {
-ch=fgetc(fp);
-putchar(ch);
-}while(ch!=EOF);
-fclose(fp);
-return(0);
+len=strlen(p);
+for(i=0;i<1000;i++)
+table[i]=len;
+for(j=0;j<=len-2;j++)
+table[p[j]]=len-1-j;
+}
+int horspool(char p[], char t[])
+{
+shift(p);
+m=strlen(p);
+n=strlen(t);
+i=m-1;
+while(i<=n-1)
+{
+k=0;
+while(k<=m-1 && (p[m-1-k]==t[i-k]))
+k++;
+if(k==m)
+return i-m+1;
+else
+i=i+table[t[i]];
+}
+return -1;
 }
